7a8371a0108113d90f7d8f493abe2d683d35f84c
Ayako <ayakonagane@gmail.com>
2025-03-11 19:17:07 +1300

    desined initial page

--- app/auth/choose.tsx
+++ app/auth/choose.tsx
@@ -1,4 +1,12 @@
-import { View, Text, Button, Image } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import { Link } from "expo-router";
@@ -6,31 +14,56 @@ import { Link } from "expo-router";
 const choose = () => {
   const router = useRouter();
   return (
-    <>
-      <View style={{ alignItems: "center" }}>
+    <SafeAreaView>
+      <View style={{ alignItems: "center", marginTop: 100 }}>
         <Image
-          source={require("../../assets/images/logo.jpg")}
+          source={require("../../assets/images/logo.png")}
           style={{
-            width: 100,
-            height: 100,
+            width: 200,
+            height: 300,
             resizeMode: "contain",
             borderRadius: 50,
           }}
         />
       </View>
-      <View>
-        <Button
-          title="Are you volunteer"
+      <View style={{ alignItems: "center" }}>
+        <TouchableOpacity
           onPress={() => router.replace("/auth/authVolunteer")}
-        />
+          style={styles.buttonStyle}
+        >
+          <Text style={styles.buttonText}>You are a volunteer</Text>
+        </TouchableOpacity>
 
-        <Button
-          title="Are you organisation"
+        <TouchableOpacity
           onPress={() => router.replace("/auth/authOrganisation")}
-        />
+          style={styles.buttonStyle}
+        >
+          <Text style={styles.buttonText}>You are a organisaiton</Text>
+        </TouchableOpacity>
       </View>
-    </>
+    </SafeAreaView>
   );
 };
 
 export default choose;
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: "#0d528f",
+    paddingVertical: 16,
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    width: "80%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
