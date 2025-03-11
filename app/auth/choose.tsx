5ee095c2bc273510a9a9c6d767f770bdb9089dc2
Ayako <ayakonagane@gmail.com>
2025-03-11 15:22:39 +1300

    added logo

--- app/auth/choose.tsx
+++ app/auth/choose.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import { Link } from "expo-router";
@@ -6,17 +6,30 @@ import { Link } from "expo-router";
 const choose = () => {
   const router = useRouter();
   return (
-    <View>
-      <Button
-        title="Are you volunteer"
-        onPress={() => router.replace("/auth/authVolunteer")}
-      />
+    <>
+      <View style={{ alignItems: "center" }}>
+        <Image
+          source={require("../../assets/images/logo.jpg")}
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+            borderRadius: 50,
+          }}
+        />
+      </View>
+      <View>
+        <Button
+          title="Are you volunteer"
+          onPress={() => router.replace("/auth/authVolunteer")}
+        />
 
-      <Button
-        title="Are you organisation"
-        onPress={() => router.replace("/auth/authOrganisation")}
-      />
-    </View>
+        <Button
+          title="Are you organisation"
+          onPress={() => router.replace("/auth/authOrganisation")}
+        />
+      </View>
+    </>
   );
 };
 
