2a1ac57ed26fde02cf2cc1621bebd143806e26a8
Ayako <ayakonagane@gmail.com>
2025-03-22 14:12:35 +1300

    id pass working to detail page

--- app/vol/more/[id].tsx
+++ app/vol/more/[id].tsx
@@ -1,9 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import { mockOpportunities } from "@/constants/mockListing";
+import { Opportunity } from "@/constants/mockListing";
+import { useEffect, useState } from "react";
 const Page = () => {
   const { id } = useLocalSearchParams();
-  return <Text>{id}</Text>;
+  const [opportunity, setOpportunity] = useState<Opportunity | null>(null);
+
+  useEffect(() => {
+    const foundOpportunity = mockOpportunities.find((opp) => opp.id === id);
+    setOpportunity(foundOpportunity || null);
+  }, [id]);
+  return <Text>{opportunity?.title}</Text>;
 };
 
 export default Page;
