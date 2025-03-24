3159dec9511a79f2e4c1e28ef4473f85fc8b9dbc
Ayako <ayakonagane@gmail.com>
2025-03-24 15:27:49 +1300

    added hook

---
+++ hooks/vol/useListing.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Opportunity } from "@/constants/mockListing";
+
+const useListing = () => {};
+
+export default useListing;
