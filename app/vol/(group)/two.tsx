0465fe5bdd429c6f771989666173820a5f10a62b
Ayako <ayakonagane@gmail.com>
2025-04-11 12:38:17 +1200

    added photo icon

--- app/vol/(group)/two.tsx
+++ app/vol/(group)/two.tsx
@@ -1,7 +1,9 @@
-import { Button } from "react-native";
+import { Button, View, Image, StyleSheet } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import useLogout from "@/hooks/vol/useLogout";
+
+const defaultProfileIcon = require("../../../assets/images/blankIcon.png");
 
 const Two = () => {
   const router = useRouter();
@@ -12,7 +14,27 @@ const Two = () => {
     router.replace("/"); // Redirect to the index page
   };
 
-  return <Button onPress={Logout} title="Logout" />;
+  return (
+    <>
+      <Image
+        source={defaultProfileIcon}
+        style={[styles.image, { width: 30, height: 30, borderRadius: 15 }]}
+      />
+      <Button onPress={Logout} title="Logout" />
+    </>
+  );
 };
 
 export default Two;
+
+const styles = StyleSheet.create({
+  avatar: {
+    overflow: "hidden",
+    backgroundColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    resizeMode: "cover",
+  },
+});
