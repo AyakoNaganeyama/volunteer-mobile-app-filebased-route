d900b6dff1655b4f52e2c9e272e6a66f76202394
Ayako <ayakonagane@gmail.com>
2025-04-11 13:07:24 +1200

    adeed profile page

--- app/vol/(group)/two.tsx
+++ app/vol/(group)/two.tsx
@@ -1,13 +1,23 @@
-import { Button, View, Image, StyleSheet } from "react-native";
+import {
+  Button,
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import useLogout from "@/hooks/vol/useLogout";
+import { useVolunteerStore } from "@/userStore/volSore";
 
 const defaultProfileIcon = require("../../../assets/images/blankIcon.png");
 
 const Two = () => {
   const router = useRouter();
   const { handleLogout } = useLogout();
+  const { volunteer } = useVolunteerStore();
 
   const Logout = async () => {
     await handleLogout(); // Execute logout logic
@@ -16,11 +26,55 @@ const Two = () => {
 
   return (
     <>
-      <Image
-        source={defaultProfileIcon}
-        style={[styles.image, { width: 30, height: 30, borderRadius: 15 }]}
-      />
-      <Button onPress={Logout} title="Logout" />
+      <View style={{ alignSelf: "center", width: "80%", marginTop: 50 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <Image
+            source={defaultProfileIcon}
+            style={[
+              styles.image,
+              { width: 100, height: 100, borderRadius: 50 },
+            ]}
+          />
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Replace Photo</Text>
+          </TouchableOpacity>
+          {/* <Text style={{ marginLeft: 10, textAlign: "center", fontSize: 18 }}>
+            {volunteer?.fullName}
+          </Text> */}
+        </View>
+
+        <View style={{ flexDirection: "column", marginTop: 20, gap: 10 }}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={volunteer?.fullName}
+              style={styles.input}
+              keyboardType="email-address"
+              editable={false}
+            />
+          </View>
+
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={volunteer?.email}
+              style={styles.input}
+              keyboardType="email-address"
+              editable={false}
+            />
+          </View>
+
+          <TouchableOpacity onPress={Logout} style={styles.buttonStyle}>
+            <Text style={styles.buttonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
@@ -36,5 +90,53 @@ const styles = StyleSheet.create({
   },
   image: {
     resizeMode: "cover",
+  },
+
+  button: {
+    backgroundColor: "#0d528f",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    borderColor: "#007aff",
+    borderRadius: 12,
+    borderWidth: 1.5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 20,
+    width: "100%",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    color: "gray",
+    fontSize: 16,
+  },
+
+  buttonStyle: {
+    backgroundColor: "#0d528f",
+    paddingVertical: 16,
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    width: "100%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
