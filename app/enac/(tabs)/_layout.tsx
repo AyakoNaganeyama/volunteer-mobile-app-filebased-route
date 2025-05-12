8d50fa9a35408467480721d8c1d2a16b3032ffda
Ayako <ayakonagane@gmail.com>
2025-05-12 12:15:29 +1200

    logout button changed

--- app/enac/(tabs)/_layout.tsx
+++ app/enac/(tabs)/_layout.tsx
@@ -1,13 +1,25 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import useLogout from "@/hooks/org/useLogout";
+import { useRouter } from "expo-router";
 
 const _layout = () => {
+  const router = useRouter();
+  const { handleLogout } = useLogout();
+  const Logout = async () => {
+    await handleLogout(); // Execute logout logic
+    router.replace("/"); // Redirect to the index page
+  };
+
   return (
     <Tabs
       screenOptions={{
+        // hide the tab bar completely
+        tabBarStyle: { display: "none" },
+
         tabBarActiveTintColor: "#0d528f",
         tabBarInactiveTintColor: "gray",
         headerTitle: () => (
@@ -17,6 +29,19 @@ const _layout = () => {
           />
         ),
         headerTitleAlign: "center",
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={Logout}
+            style={{
+              marginRight: 16,
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Ionicons name="log-out-outline" size={20} color="#0d528f" />
+            <Text style={{ color: "#0d528f", marginLeft: 4 }}>Logout</Text>
+          </TouchableOpacity>
+        ),
       }}
     >
       <Tabs.Screen
@@ -24,7 +49,7 @@ const _layout = () => {
         options={{
           tabBarLabel: "Manage",
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="post-add" size={24} color={color} />
+            <MaterialIcons name="post-add" size={size} color={color} />
           ),
         }}
       />
@@ -33,7 +58,7 @@ const _layout = () => {
         options={{
           tabBarLabel: "Profile",
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-circle-outline" size={24} color={color} />
+            <Ionicons name="person-circle-outline" size={size} color={color} />
           ),
         }}
       />
